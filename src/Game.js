dcd0e14527dbe8b7ce73f703e3e2e3675e248b58
ngaivoronski <ngaivoronski@gmail.com>
2019-10-21 13:34:08 -0400

    set up

---
+++ src/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Game() {
+  return (
+    <div className="App">
+    <h1>Hello World!</h1>
+
+
+    </div>
+  );
+}
+
+export default Game;
